8fb99dbf5217a9604f9200b88beba1a03f251936
AquariusCorsair <AquariusCorsair@outlook.com>
2017-04-22 17:24:24 -0400

    flask

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ install
 python 2.7
 pip 9.0.1
 flask 0.12.1
+
+# Flask-MySQLdb doc
+http://flask-mysqldb.readthedocs.io/en/latest/
--- app.py
+++ app.py
@@ -1,9 +1,43 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_mysqldb import MySQL
 
-@app.route("/")
-def hello():
-	return "Hello World"
+from flask import render_template, request
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_HOST'] = 'localhost'
+mysql = MySQL()
+mysql.init_app(app)
+
+# @app.route('/create_token/<token_name>/<password>')
+# def add_token():
+# 	if password != "hackru":
+# 		return render_template('index.html')
+# 	cur = mysql.connection.cursor()
+#     cur.execute('''
+#     	CREATE TABLE token_name (
+#     		id int, 
+#     		name varchar(255),
+#     		token varchar(255)
+# 		);''')
+#     # download data from finance api and save as draw.csv
+#     # fill data to the database of new token
+#     return render_template('priceChart.html')
+
+@app.route('/')
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute(''' use stockr''') 
+    return render_template('index.html')
+
+@app.route('/draw',methods=['POST'])
+def get_token():
+	token=request.form['token_name']
+	return str(token)
+	# cur = mysql.connection.cursor()
+
 
 if __name__ == "__main__":
 	app.run()
