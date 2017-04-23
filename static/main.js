a4d90aa19e7b10820fee7f779278a04872e7255a
candicelkf <lkfcp3@gmail.com>
2017-04-22 23:44:23 -0400

    modified

--- static/main.js
+++ static/main.js
@@ -1,13 +1,23 @@
+//$('#btn').click(function() {
+//    $.ajax({
+//        type : 'POST',
+//        url : '/',
+//        data : $('#symbol1').serialize()        
+//    });  
+//});
 $(document).ready(function() {
     console.log("ready!");
 	$('form').on('submit',function(event){ 
-		$.ajax ({
+		$.ajax({
 			data : {
-				symbol : $('symbol').val()
+				symbol : $('#symbol1').val()
 			},
 			type : 'POST',
 			url : '/'
-		})
+		});
 		event.preventDefault();
 	});
 });
+//$(document).ready(function() { 
+
+//});
