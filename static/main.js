df1eaca0878d6eb00e96d76fbe1753ecfdf25d97
JIANQIN GAO <jianqingao@nwk-155-32.nwk.ruw.rutgers.edu>
2017-04-23 11:00:17 -0400

    basically implement jquery ajax request get stock price chart

--- static/main.js
+++ static/main.js
@@ -5,19 +5,38 @@
 //        data : $('#symbol1').serialize()        
 //    });  
 //});
-$(document).ready(function() {
-    console.log("ready!");
-	$('form').on('submit',function(event){ 
-		$.ajax({
-			data : {
-				symbol : $('#symbol1').val()
-			},
-			type : 'POST',
-			url : '/'
-		});
-		event.preventDefault();
-	});
-});
+// $(document).ready(function() {
+//     console.log("ready!");
+// 	$('form').on('submit',function(event){ 
+// 		$.ajax({
+// 			data : {
+// 				symbol : $('#symbol1').val()
+// 			},
+// 			type : 'POST',
+// 			url : '/'
+// 		});
+// 		event.preventDefault();
+// 	});
+// });
 //$(document).ready(function() { 
 
 //});
+
+$(document).ready(function() {
+	$('#btn').click(function() {
+		var symbol = $('#symbol1').val();
+		$('form').attr('action', '/'+ symbol);
+
+		$.ajax({
+			url: '/' + symbol,
+			data: $('form').serialize(),
+			type: 'POST',
+			success: function(response) {
+				console.log(response);
+			},
+			error: function(error) {
+				console.log(error);
+			}
+		});
+	});
+});
